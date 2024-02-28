9e1e49d2a798b6ae49c8a9d4a86c3c1fff53ff21
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2024-02-28 05:06:35 +0000

    fix format

--- web/src/components/PTeamCreateModal.jsx
+++ web/src/components/PTeamCreateModal.jsx
@@ -58,7 +58,7 @@ export function PTeamCreateModal(props) {
         const resp = error.response;
         enqueueSnackbar(
           `Operation failed: ${resp.status} ${resp.statusText} - ${resp.data?.detail}`,
-          { variant: "error" }
+          { variant: "error" },
         );
       });
   };
