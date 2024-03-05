f084d14918c972ae25c56c1e07fd5060bc9a081b
Kazuaki Anami <kazuaki@soum.co.jp>
2024-03-05 15:15:49 +0900

    add test for auto closed topic

--- api/app/tests/medium/test_alert.py
+++ api/app/tests/medium/test_alert.py
@@ -208,15 +208,109 @@ def test_pick_alert_target_for_new_topic__threshold(testdb) -> None:
     assert _find_expected(alert_targets, 3, child_tag11)
     assert _find_expected(alert_targets, 4, child_tag11)
 
-    # topic0: threshold=1
+    # topic0: threshold=3
     topic = create_topic(USER1, _gen_topic_params(3))
     alert_targets = _pick_alert_targets_for_new_topic(testdb, topic.topic_id)
     assert len(alert_targets) == 2
     assert _find_expected(alert_targets, 3, child_tag11)
     assert _find_expected(alert_targets, 4, child_tag11)
 
-    # topic0: threshold=1
+    # topic0: threshold=4
     topic = create_topic(USER1, _gen_topic_params(4))
     alert_targets = _pick_alert_targets_for_new_topic(testdb, topic.topic_id)
     assert len(alert_targets) == 1
     assert _find_expected(alert_targets, 4, child_tag11)
+
+
+def test_pick_alert_target_for_new_topic__auto_closed(testdb) -> None:
+    create_user(USER1)
+    parent_tag1 = create_tag(USER1, "pkg1:info1:")
+    child_tag11 = create_tag(USER1, "pkg1:info1:mgr1")
+    parent_tag2 = create_tag(USER1, "pkg2:info1:")
+    child_tag21 = create_tag(USER1, "pkg2:info1:mgr1")
+
+    def _gen_pteam_params(idx: int) -> dict:
+        return {
+            "pteam_name": f"pteam{idx}",
+            "alert_slack": {
+                "enable": True,
+                "webhook_url": SAMPLE_SLACK_WEBHOOK_URL + str(idx),
+            },
+            "alert_mail": {
+                "enable": True,
+                "address": f"account{idx}@example.com",
+            },
+            "alert_threat_impact": DEFAULT_ALERT_THREAT_IMPACT,
+        }
+
+    def _gen_topic_params(tags: List[schemas.TagResponse]) -> dict:
+        topic_id = str(uuid4())
+        return {
+            "topic_id": topic_id,
+            "title": "test topic " + topic_id,
+            "abstract": "test abstract " + topic_id,
+            "threat_impact": 1,
+            "tags": [tag.tag_name for tag in tags],
+            "misp_tags": [],
+            "zone_names": [],
+            "actions": [],
+        }
+
+    def _find_expected(
+        _targets: Sequence[models.CurrentPTeamTopicTagStatus],
+        idx: int,
+        tag: schemas.TagResponse,
+    ) -> bool:
+        return any(
+            _tgt.pteam.pteam_name == f"pteam{idx}" and _tgt.tag.tag_name == tag.tag_name
+            for _tgt in _targets
+        )
+
+    pteam0 = create_pteam(USER1, _gen_pteam_params(0))
+    ext_tags = {
+        child_tag11.tag_name: [("api/Pipfile.lock", "1.0.0")],
+        child_tag21.tag_name: [("api/Pipfile.lock", "1.0.0")],
+    }
+    upload_pteam_tags(USER1, pteam0.pteam_id, GROUP1, ext_tags)
+
+    action1_closable = {
+        "action": "action one",
+        "action_type": models.ActionType.elimination,
+        "recommended": True,
+        "ext": {
+            "tags": [child_tag11.tag_name],
+            "vulnerable_versions": {child_tag11.tag_name: ["< 1.0.0"]},  # closable
+        },
+        "zone_names": [],
+    }
+    action2_unclosable = {
+        "action": "action two",
+        "action_type": models.ActionType.elimination,
+        "recommended": True,
+        "ext": {
+            "tags": [child_tag21.tag_name],
+            "vulnerable_versions": {child_tag21.tag_name: ["< 2.0.0"]},  # unclosable
+        },
+        "zone_names": [],
+    }
+
+    # topic0: has parent_tag1 with closable action1
+    topic = create_topic(USER1, _gen_topic_params([parent_tag1]), actions=[action1_closable])
+    alert_targets = _pick_alert_targets_for_new_topic(testdb, topic.topic_id)
+    assert alert_targets == []
+
+    # topic1: has parent_tag2 with unclosable action2
+    topic = create_topic(USER1, _gen_topic_params([parent_tag2]), actions=[action2_unclosable])
+    alert_targets = _pick_alert_targets_for_new_topic(testdb, topic.topic_id)
+    assert len(alert_targets) == 1
+    assert _find_expected(alert_targets, 0, child_tag21)
+
+    # topic2: complex
+    topic = create_topic(
+        USER1,
+        _gen_topic_params([parent_tag1, parent_tag2]),
+        actions=[action1_closable, action2_unclosable],
+    )
+    alert_targets = _pick_alert_targets_for_new_topic(testdb, topic.topic_id)
+    assert len(alert_targets) == 1
+    assert _find_expected(alert_targets, 0, child_tag21)  # alert only uncompleted
