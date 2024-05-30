1de2607a1f634b32ede4fed3f0ab6f3d800dc221
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2024-05-30 04:47:10 +0000

    delete create_threat

--- api/app/routers/threat.py
+++ api/app/routers/threat.py
@@ -1,17 +1,10 @@
-from datetime import datetime
 from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Query, Response, status
 from sqlalchemy.orm import Session
 
-from app import models, persistence, schemas
-from app.alert import send_alert_to_pteam
-from app.common import (
-    threat_meets_condition_to_create_ticket,
-    ticket_meets_condition_to_create_alert,
-)
+from app import persistence, schemas
 from app.database import get_db
-from app.ssvc import calculate_ssvc_deployer_priority
 
 router = APIRouter(prefix="/threats", tags=["threats"])
 
@@ -32,52 +25,6 @@ def get_threats(
     """
     threats = persistence.search_threats(db, dependency_id, topic_id)
     return threats
-
-
-@router.post("", response_model=schemas.ThreatResponse)
-def create_threat(
-    data: schemas.ThreatRequest,
-    db: Session = Depends(get_db),
-):
-    now = datetime.now()
-    dependency = persistence.get_dependency_by_id(db, data.dependency_id)
-    topic = persistence.get_topic_by_id(db, data.topic_id)
-
-    if dependency is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such dependency")
-
-    if topic is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No such topic")
-
-    if persistence.search_threats(db, data.dependency_id, data.topic_id):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Threat already exists")
-
-    threat = models.Threat(
-        dependency_id=str(data.dependency_id),
-        topic_id=str(data.topic_id),
-    )
-    persistence.create_threat(db, threat)
-
-    if threat_meets_condition_to_create_ticket(db, threat):
-        ticket = models.Ticket(
-            threat_id=threat.threat_id,
-            created_at=now,
-            updated_at=now,
-            ssvc_deployer_priority=calculate_ssvc_deployer_priority(threat, threat.dependency),
-        )
-        persistence.create_ticket(db, ticket)
-        if ticket_meets_condition_to_create_alert(ticket):
-            alert = models.Alert(
-                ticket_id=ticket.ticket_id,
-                alerted_at=now,
-                alert_content=topic.hint_for_action,
-            )
-            persistence.create_alert(db, alert)
-            send_alert_to_pteam(alert)
-
-    db.commit()
-
-    return threat
 
 
 @router.get("/{threat_id}", response_model=schemas.ThreatResponse)
