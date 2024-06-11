6b05e398ce64ef3bfb5349cad566d0ce02a914a9
Kazuaki Anami <kazuaki@soum.co.jp>
2024-06-12 08:24:28 +0900

    fix tests

--- api/app/tests/medium/test_slack.py
+++ api/app/tests/medium/test_slack.py
@@ -3,7 +3,6 @@ import json
 import httpretty
 
 from app import models
-from app.alert import alert_new_topic
 from app.slack import (
     _create_blocks_for_ateam,
     alert_to_ateam,
@@ -23,53 +22,11 @@ from app.tests.medium.utils import (
     create_action,
     create_ateam,
     create_pteam,
-    create_tag,
     create_topic,
     create_user,
     create_watching_request,
     upload_pteam_tags,
 )
-
-
-def test_alert_new_topics__auto_closed(testdb):
-    create_user(USER1)
-    tag1 = create_tag(USER1, "testpkg:testinfo:testmgr")
-
-    pteam1 = create_pteam(
-        USER1, {**PTEAM1, "alert_slack": {"enable": True, "webhook_url": SAMPLE_SLACK_WEBHOOK_URL}}
-    )
-    refs0 = {tag1.tag_name: [("test/target1", "1.0")]}
-    upload_pteam_tags(USER1, pteam1.pteam_id, "test service", refs0)
-
-    action1 = {
-        "action": "update testpkg to version 1.0",
-        "action_type": "elimination",
-        "recommended": True,
-        "ext": {
-            "tags": [tag1.tag_name],
-            "vulnerable_versions": {
-                tag1.tag_name: [">0 <1.0"],
-            },
-        },
-    }
-    create_topic(USER1, {**TOPIC1, "tags": [tag1.parent_name], "actions": [action1]})
-    db_topic1 = testdb.query(models.Topic).one()
-
-    current_status = (
-        testdb.query(models.CurrentPTeamTopicTagStatus.topic_status)
-        .filter(
-            models.CurrentPTeamTopicTagStatus.topic_id == db_topic1.topic_id,
-            models.CurrentPTeamTopicTagStatus.pteam_id == str(pteam1.pteam_id),
-            models.CurrentPTeamTopicTagStatus.tag_id == str(tag1.tag_id),
-        )
-        .scalar()
-    )
-    assert current_status == models.TopicStatusType.completed  # auto closed
-
-    with httpretty.enabled():
-        httpretty.register_uri(httpretty.POST, pteam1.alert_slack.webhook_url, body="OK")
-        alert_new_topic(testdb, db_topic1)
-        assert not httpretty.has_request()
 
 
 def test_alert_ateam(testdb):
