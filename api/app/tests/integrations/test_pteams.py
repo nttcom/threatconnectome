6639e020606c8fffa2178e4b9ad1aa46b9779d33
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2024-06-27 07:55:09 +0000

    fix test name

--- api/app/tests/integrations/test_pteams.py
+++ api/app/tests/integrations/test_pteams.py
@@ -23,12 +23,10 @@ client = TestClient(app)
 @pytest.mark.parametrize(
     "topic_status, solved_num, unsolved_num",
     [
-        (models.TopicStatusType.acknowledged, 0, 1),
-        (models.TopicStatusType.scheduled, 0, 1),
         (models.TopicStatusType.completed, 1, 0),
     ],
 )
-def test_get_service_tagged_ticket_ids_when_change_the_topic_status_type(
+def test_it_should_return_solved_number_based_on_ticket_status(
     testdb, topic_status, solved_num, unsolved_num
 ):
 
@@ -49,7 +47,58 @@ def test_get_service_tagged_ticket_ids_when_change_the_topic_status_type(
         json_data,
     )
 
-    # create current_ticket_status table
+    response = client.get(
+        f"/pteams/{ticket_response['pteam_id']}/services/{ticket_response['service_id']}/tags/{ticket_response['tag_id']}/ticket_ids",
+        headers=headers(USER1),
+    )
+    assert response.status_code == 200
+    response = response.json()
+
+    # solved
+    assert response["solved"]["pteam_id"] == ticket_response["pteam_id"]
+    assert response["solved"]["service_id"] == ticket_response["service_id"]
+    assert response["solved"]["tag_id"] == ticket_response["tag_id"]
+    assert len(response["solved"]["topic_ticket_ids"]) == solved_num
+    assert (
+        ticket_response["ticket_id"] in response["solved"]["topic_ticket_ids"][0]["ticket_ids"][0]
+    )
+    assert response["solved"]["topic_ticket_ids"][0]["topic_id"] == ticket_response["topic_id"]
+
+    # unsolved
+    assert response["unsolved"]["pteam_id"] == ticket_response["pteam_id"]
+    assert response["unsolved"]["service_id"] == ticket_response["service_id"]
+    assert response["unsolved"]["tag_id"] == ticket_response["tag_id"]
+    assert len(response["unsolved"]["topic_ticket_ids"]) == unsolved_num
+
+
+@pytest.mark.parametrize(
+    "topic_status, solved_num, unsolved_num",
+    [
+        (models.TopicStatusType.acknowledged, 0, 1),
+        (models.TopicStatusType.scheduled, 0, 1),
+    ],
+)
+def test_it_should_return_unsolved_number_based_on_ticket_status(
+    testdb, topic_status, solved_num, unsolved_num
+):
+
+    ticket_response = ticket_utils.create_ticket(testdb, USER1, PTEAM1, TOPIC1, ACTION1)
+
+    json_data = {
+        "topic_status": topic_status,
+        "note": "string",
+        "assignees": [],
+        "scheduled_at": str(datetime.now()),
+    }
+    create_service_topicstatus(
+        USER1,
+        ticket_response["pteam_id"],
+        ticket_response["service_id"],
+        ticket_response["topic_id"],
+        ticket_response["tag_id"],
+        json_data,
+    )
+
     response = client.get(
         f"/pteams/{ticket_response['pteam_id']}/services/{ticket_response['service_id']}/tags/{ticket_response['tag_id']}/ticket_ids",
         headers=headers(USER1),
@@ -63,27 +112,15 @@ def test_get_service_tagged_ticket_ids_when_change_the_topic_status_type(
     assert response["solved"]["tag_id"] == ticket_response["tag_id"]
     assert len(response["solved"]["topic_ticket_ids"]) == solved_num
 
-    if solved_num == 1:
-        assert (
-            ticket_response["ticket_id"]
-            in response["solved"]["topic_ticket_ids"][0]["ticket_ids"][0]
-        )
-        assert response["solved"]["topic_ticket_ids"][0]["topic_id"] == ticket_response["topic_id"]
-
     # unsolved
     assert response["unsolved"]["pteam_id"] == ticket_response["pteam_id"]
     assert response["unsolved"]["service_id"] == ticket_response["service_id"]
     assert response["unsolved"]["tag_id"] == ticket_response["tag_id"]
     assert len(response["unsolved"]["topic_ticket_ids"]) == unsolved_num
-
-    if unsolved_num == 1:
-        assert (
-            ticket_response["ticket_id"]
-            in response["unsolved"]["topic_ticket_ids"][0]["ticket_ids"][0]
-        )
-        assert (
-            response["unsolved"]["topic_ticket_ids"][0]["topic_id"] == ticket_response["topic_id"]
-        )
+    assert (
+        ticket_response["ticket_id"] in response["unsolved"]["topic_ticket_ids"][0]["ticket_ids"][0]
+    )
+    assert response["unsolved"]["topic_ticket_ids"][0]["topic_id"] == ticket_response["topic_id"]
 
 
 @pytest.mark.parametrize(
@@ -139,7 +176,7 @@ def test_get_service_tagged_ticket_ids_when_change_the_topic_status_type(
         ),
     ],
 )
-def test_get_service_tagged_ticket_ids_when_change_the_threat_impact(
+def test_it_shoud_return_threat_impact_count_num_based_on_tickte_status(
     testdb, threat_impact, topic_status, solved_threat_impact_count, unsolved_threat_impact_count
 ):
     topic = {
@@ -164,7 +201,6 @@ def test_get_service_tagged_ticket_ids_when_change_the_threat_impact(
         json_data,
     )
 
-    # create current_ticket_status table
     response = client.get(
         f"/pteams/{ticket_response['pteam_id']}/services/{ticket_response['service_id']}/tags/{ticket_response['tag_id']}/ticket_ids",
         headers=headers(USER1),
