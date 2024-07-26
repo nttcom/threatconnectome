7883231db7f424c17259dddaff5fa1f4c944f7ba
mshim03 <35719050+mshim03@users.noreply.github.com>
2024-07-26 13:02:54 +0900

    use create_topic_tag in get_or_create_topic_tag

--- api/app/common.py
+++ api/app/common.py
@@ -149,22 +149,7 @@ def get_or_create_topic_tag(db: Session, tag_name: str) -> models.Tag:
     if tag := persistence.get_tag_by_name(db, tag_name):  # already exists
         return tag
 
-    tag = models.Tag(tag_name=tag_name, parent_id=None, parent_name=None)
-    if not (parent_name := _pick_parent_tag(tag_name)):  # no parent: e.g. "tag1"
-        persistence.create_tag(db, tag)
-        return tag
-
-    if parent_name == tag_name:  # parent is myself
-        tag.parent_id = tag.tag_id
-        tag.parent_name = tag_name
-    else:
-        parent = get_or_create_topic_tag(db, parent_name)
-        tag.parent_id = parent.tag_id
-        tag.parent_name = parent.tag_name
-
-    persistence.create_tag(db, tag)
-
-    return tag
+    return create_topic_tag(db, tag_name)
 
 
 def create_topic_tag(db: Session, tag_name: str) -> models.Tag:
