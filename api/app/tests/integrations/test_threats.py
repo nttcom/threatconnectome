c340789aeb857a75b02ca3e478b6ce910bc486c3
Shikou <shiko.dejima.uw@hitachi-solutions.com>
2024-08-05 11:02:56 +0900

    fix ticket when delete action

--- api/app/tests/integrations/test_threats.py
+++ api/app/tests/integrations/test_threats.py
@@ -1,7 +1,9 @@
 import pytest
+from fastapi.testclient import TestClient
 from sqlalchemy import select
 
 from app import models
+from app.main import app
 from app.tests.medium.constants import (
     PTEAM1,
     TOPIC1,
@@ -12,9 +14,12 @@ from app.tests.medium.utils import (
     create_tag,
     create_topic,
     create_user,
+    headers,
     update_topic,
     upload_pteam_tags,
 )
+
+client = TestClient(app)
 
 
 class TestFixThreatsForTopic:
@@ -164,6 +169,77 @@ class TestFixThreatsForTopic:
                 assert db_threat1.ticket is None
         else:
             assert len(db_dependency1.threats) == 0
+
+    @pytest.mark.parametrize(
+        "vuln_versions_1, vuln_versions_2, delete_action_1, delete_action_2, should_exist_ticket",
+        [
+            (["< 1.5"], ["< 1.0"], False, True, True),
+            (["< 1.0"], ["< 1.5"], False, True, False),
+            (["< 1.0"], ["< 1.5"], True, True, False),
+        ],
+    )
+    def test_threat_ticket_creation_on_delete_action(
+        self,
+        testdb,
+        vuln_versions_1,
+        vuln_versions_2,
+        delete_action_1,
+        delete_action_2,
+        should_exist_ticket,
+    ):
+        dep_version = "1.2"
+
+        create_user(USER1)
+        tag1 = create_tag(USER1, "foobar:ubuntu-24.04:")
+        pteam1 = create_pteam(USER1, PTEAM1)
+        refs0 = {tag1.tag_name: [("test target", dep_version)]}  # dependency has tag1
+        service_name = "test service"
+        upload_pteam_tags(USER1, pteam1.pteam_id, service_name, refs0)
+
+        action1 = {
+            "action": "test action",
+            "action_type": models.ActionType.elimination,
+            "recommended": True,
+            "ext": {
+                "tags": [tag1.tag_name],
+                "vulnerable_versions": {tag1.tag_name: vuln_versions_1},
+            },
+        }
+        action2 = {
+            "action": "test action2",
+            "action_type": models.ActionType.elimination,
+            "recommended": True,
+            "ext": {
+                "tags": [tag1.tag_name],
+                "vulnerable_versions": {tag1.tag_name: vuln_versions_2},
+            },
+        }
+        # create topic1 with actionable tag1 -- having threat & ticket at this time
+        topic1 = create_topic(
+            USER1, {**TOPIC1, "tags": [tag1.tag_name], "actions": [action1, action2]}
+        )
+
+        if delete_action_1:
+            client.delete(
+                f"/actions/{topic1.actions[0].action_id}",
+                headers=headers(USER1),
+            )
+        if delete_action_2:
+            client.delete(
+                f"/actions/{topic1.actions[1].action_id}",
+                headers=headers(USER1),
+            )
+
+        db_dependency1 = testdb.scalars(
+            select(models.Dependency)
+            .join(models.Service)
+            .where(models.Service.pteam_id == str(pteam1.pteam_id))
+        ).one()
+
+        assert len(db_dependency1.threats) == 1
+        db_threat1 = db_dependency1.threats[0]
+        assert db_threat1.topic_id == str(topic1.topic_id)
+        assert (db_threat1.ticket is not None) == should_exist_ticket
 
 
 class TestFixThreatsForDependency:
