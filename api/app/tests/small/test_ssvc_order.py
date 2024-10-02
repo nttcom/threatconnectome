7df3f9c9fa7ee4ba16d3898acd8c6b9c912c63ef
Kazuaki Anami <kazuaki@soum.co.jp>
2024-10-02 10:21:59 +0900

    append tests for comparison operators

--- api/app/tests/small/test_ssvc_order.py
+++ api/app/tests/small/test_ssvc_order.py
@@ -110,3 +110,29 @@ def test_ssvc_priority_enum_comparison_with_different_type():
 )
 def test_ssvc_priority_enum_le(ssvc_priorityA, ssvc_priorityB, expected):
     assert (ssvc_priorityA <= ssvc_priorityB) == expected
+
+
+[_immediate, _out_of_cycle, _scheduled, _defer] = list(models.SSVCDeployerPriorityEnum)
+
+
+@pytest.mark.parametrize("right", [_immediate, _out_of_cycle, _scheduled, _defer])
+@pytest.mark.parametrize("operator", [">", ">=", "==", "!=", "<=", "<"])
+@pytest.mark.parametrize("left", [_immediate, _out_of_cycle, _scheduled, _defer])
+def test_comparison_operators(left, right, operator):
+    expected_order = {_immediate: 1, _out_of_cycle: 2, _scheduled: 3, _defer: 4}
+    left_order = expected_order[left]
+    right_order = expected_order[right]
+
+    operators_equal = {">=", "==", "<="}
+    operators_small = {"<", "<=", "!="}
+    operators_large = {">", ">=", "!="}
+
+    if left_order == right_order:
+        expected_true_operators = operators_equal
+    elif left_order < right_order:
+        expected_true_operators = operators_small
+    else:
+        expected_true_operators = operators_large
+
+    expected = operator in expected_true_operators
+    assert eval(f"left {operator} right") is expected
