df41c4b9bd6520a18d06ff65bdcf3eefd8a1535f
chisaka12 <t.chisaka@ntt.com>
2024-10-07 09:54:33 +0900

    code modification

--- web/src/components/PTeamStatusSSVCCards.jsx
+++ web/src/components/PTeamStatusSSVCCards.jsx
@@ -114,13 +114,13 @@ export function PTeamStatusSSVCCards(props) {
             >
               {HighestSSVCPriorityList.items.map((item) =>
                 item === highestSsvcPriority ? (
-                  <ToggleButton key={item} value={item} sx={{ padding: "0" }} disabled>
+                  <ToggleButton key={item} value={item} sx={{ padding: "0" }}>
                     <Button startIcon={<Icon />} sx={{ color: "white" }}>
                       {HighestSSVCPriorityList.valuePairing[item]}
                     </Button>
                   </ToggleButton>
                 ) : (
-                  <ToggleButton key={item} value={item} sx={{ padding: "0" }} disabled>
+                  <ToggleButton key={item} value={item} sx={{ padding: "0" }}>
                     <Button disabled>{HighestSSVCPriorityList.valuePairing[item]}</Button>
                   </ToggleButton>
                 ),
