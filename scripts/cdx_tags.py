adc5820eeb0f16f6d566dd96d21663a7191f497b
Shikou <shiko.dejima.uw@hitachi-solutions.com>
2024-10-11 11:59:27 +0900

    rollback incorrect fix

--- scripts/cdx_tags.py
+++ scripts/cdx_tags.py
@@ -3,11 +3,7 @@ import json
 import os
 import re
 import sys
-from typing import (
-    Any,
-    ClassVar,
-    Pattern,
-)
+from typing import Any, ClassVar, Pattern
 
 from cyclonedx.exception import MissingOptionalDependencyException
 from cyclonedx.schema import SchemaVersion
