b6886480e4f347b8e41ca072b5c99875ab0b1eca
Shikou <shiko.dejima.uw@hitachi-solutions.com>
2024-11-13 15:10:45 +0900

    set team state when common page

--- web/src/pages/App.jsx
+++ web/src/pages/App.jsx
@@ -110,6 +110,14 @@ export function App() {
         return;
       }
       setPteamId(pteamIdx);
+    } else if (["/topics", "/account"].includes(location.pathname)) {
+      if (params.get("ateamId")) {
+        dispatch(setTeamMode("ateam"));
+        setATeamId(params.get("ateamId"));
+      } else if (params.get("pteamId")) {
+        dispatch(setTeamMode("pteam"));
+        setPteamId(params.get("pteamId"));
+      }
     }
   }, [dispatch, enqueueSnackbar, navigate, location, userMe, userMeIsFetching, system.teamMode]);
 
