d18d11da6d005999c4f9617f692fdd28867c0674
chisaka12 <t.chisaka@ntt.com>
2024-12-09 16:15:26 +0900

    add alembic upgrade head process to the demo setup script

--- set_up_demo_environment/set_up.sh
+++ set_up_demo_environment/set_up.sh
@@ -7,6 +7,7 @@ sudo cp ./accounts.json ../firebase/data/auth_export/accounts.json
 docker compose -f ../docker-compose-local.yml up --build -d
 docker compose -f ../docker-compose-local.yml cp demo_data db:/
 docker compose -f ../docker-compose-local.yml exec -T db pg_restore --clean --if-exists -U postgres -d postgres demo_data
+sudo docker compose -f docker-compose-local.yml exec api sh -c "cd app && alembic upgrade head"
 cd ../web
 npm ci && npm run build
 docker compose -f ../docker-compose-local.yml restart
