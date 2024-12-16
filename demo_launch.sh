fdd54a40489fbe4d865f80516d8dad8b9ba6cdf1
Masaki Shimura <35719050+mshim03@users.noreply.github.com>
2024-12-16 10:43:11 +0900

    Revert "refine one liner for demo"

--- demo_launch.sh
+++ demo_launch.sh
@@ -2,7 +2,8 @@
 
 docker_yml="docker-compose-demo.yml";
 
-cmd="docker compose -f ${docker_yml} down -v";
+cmd="docker compose -f ${docker_yml} up -d";
 echo "${cmd}";
+
 # shellcheck disable=SC2086
 exec ${cmd};
