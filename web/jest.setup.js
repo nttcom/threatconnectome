c4df2cd6e07832fa37853a48a8acfd751d6c15d7
Kazuaki Anami <kazuaki@soum.co.jp>
2025-01-10 13:02:13 +0900

    remove currently not needed fix

--- web/jest.setup.js
+++ web/jest.setup.js
@@ -1,9 +1,1 @@
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
-import { TextDecoder, TextEncoder, ReadableStream, Request, Response } from "node:util";
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-global.ReadableStream = ReadableStream;
-global.Request = Request;
-global.Response = Response;
