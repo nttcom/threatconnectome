3e54aa3b6368c46c9bd3715db7dbc76991443c46
Kazuaki Anami <kazuaki@soum.co.jp>
2025-01-22 12:37:55 +0900

    fix eslint.config.js to ignore build directory

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', "build"] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
