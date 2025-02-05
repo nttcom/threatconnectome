ff7872e9530110815bd9fe09375c916103f70480
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2025-02-05 05:10:04 +0000

    Change import name

--- web/src/pages/Status/StatusPage.jsx
+++ web/src/pages/Status/StatusPage.jsx
@@ -28,7 +28,7 @@ import { grey } from "@mui/material/colors";
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { useLocation, useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { Android12Switch } from "../../components/Android12Switch";
 import { PTeamLabel } from "../../components/PTeamLabel";
