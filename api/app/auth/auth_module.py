75817ff37df83eaaa8d8ca35eb864ba65d5fddc4
Shikou <shiko.dejima.uw@hitachi-solutions.com>
2025-02-12 13:07:52 +0900

    change auth api

--- api/app/auth/auth_module.py
+++ api/app/auth/auth_module.py
@@ -1,3 +1,6 @@
+from ..schemas import Token
+
+
 def get_auth_module():
     return AuthModule(None)
 
@@ -5,6 +8,12 @@ def get_auth_module():
 class AuthModule:
     def __init__(self):
         pass
+
+    def login_for_access_token(self, username, password) -> Token:
+        return Token(access_token="", token_type="bearer", refresh_token="")
+
+    def refresh_access_token(self, refresh_token) -> Token:
+        return Token(access_token="", token_type="bearer", refresh_token="")
 
     def check_and_get_user_info(self, token):
         pass
--- api/app/auth/firebase_auth_module.py
+++ api/app/auth/firebase_auth_module.py
@@ -1,9 +1,13 @@
+import json
 import os
 
+import requests
 from fastapi import HTTPException, status
 from firebase_admin import auth, credentials, initialize_app
 
 from app.auth.auth_module import AuthModule
+
+from ..schemas import Token
 
 
 class FirebaseAuthModule(AuthModule):
@@ -11,6 +15,92 @@ class FirebaseAuthModule(AuthModule):
         super().__init__()
         self.cred = credentials.Certificate(os.environ["FIREBASE_CRED"])
         initialize_app(self.cred)
+
+    def login_for_access_token(self, username, password) -> Token:
+        payload = {
+            "email": username,
+            "password": password.get_secret_value(),
+            "returnSecureToken": True,
+        }
+
+        api_key = os.getenv("FIREBASE_API_KEY", "")
+        # https://github.com/firebase/firebase-admin-python/blob/master/firebase_admin/_auth_utils.py
+        id_toolkit = "identitytoolkit.googleapis.com/v1"
+        sign_in_url_without_scheme = f"{id_toolkit}/accounts:signInWithPassword?key={api_key}"
+        emulator_host = os.getenv("FIREBASE_AUTH_EMULATOR_HOST", "")
+        if emulator_host != "":
+            sign_in_url = f"http://{emulator_host}/{sign_in_url_without_scheme}"
+        else:
+            sign_in_url = f"https://{sign_in_url_without_scheme}"
+
+        try:
+            resp = requests.post(
+                sign_in_url,
+                json.dumps(payload),
+                headers={"Content-Type": "application/json"},
+                timeout=30,
+            )
+        except requests.exceptions.Timeout as firebase_timeout:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            ) from firebase_timeout
+
+        data = resp.json()
+        if not resp.ok:
+            error_message = data["error"]["message"]
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=error_message if error_message else "Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return Token(
+            access_token=data["idToken"], token_type="bearer", refresh_token=data["refreshToken"]
+        )
+
+    def refresh_access_token(self, refresh_token) -> Token:
+        payload = {
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token,
+        }
+
+        # see https://firebase.google.com/docs/reference/rest/auth#section-refresh-token
+        api_key = os.environ["FIREBASE_API_KEY"]
+        refresh_path = f"securetoken.googleapis.com/v1/token?key={api_key}"
+        emulator_host = os.getenv("FIREBASE_AUTH_EMULATOR_HOST", "")
+        if emulator_host != "":
+            refresh_token_url = f"http://{emulator_host}/{refresh_path}"
+        else:
+            refresh_token_url = f"https://{refresh_path}"
+
+        try:
+            resp = requests.post(
+                refresh_token_url,
+                json.dumps(payload),
+                headers={"Content-Type": "application/json"},
+                timeout=30,
+            )
+        except requests.exceptions.Timeout as firebase_timeout:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Could not refresh token",
+                headers={"WWW-Authenticate": "Bearer"},
+            ) from firebase_timeout
+
+        data: dict = resp.json()
+        if not resp.ok:
+            error_message: str = data["error"]["message"]
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=error_message if error_message else "Could not refresh token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return Token(
+            access_token=data["id_token"],
+            token_type=data["token_type"],
+            refresh_token=data["refresh_token"],
+        )
 
     def check_and_get_user_info(self, token):
         try:
