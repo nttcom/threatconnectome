28d687e9469b44efe96a10d16bbed86cd9f48107
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2025-02-19 05:02:41 +0000

    fix path

--- testapi_supabase.sh
+++ testapi_supabase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-tar -jxvf ./volumes/db/data-test.tar.bz2 -C ./volumes/db/
+tar -jxvf ./supabase/volumes/db/data-test.tar.bz2 -C ./supabase/volumes/db/
 docker compose -f docker-compose-test-supabase.yml up --build -d
 docker compose -f docker-compose-test-supabase.yml exec testapi pytest -vv app/tests/ # --cov --cov-branch --cov-report=term-missing --cov-config=app/tests/.coveragerc
 docker compose -f docker-compose-test-supabase.yml down -v
-sudo rm -r ./volumes/db/data-test
+sudo rm -r ./supabase/volumes/db/data-test
