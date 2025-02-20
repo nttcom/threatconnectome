3516b84d7cf5b69f026006a82e6befcda887b4c7
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2025-02-20 00:11:23 +0000

    fix path in sh script

--- testapi_supabase.sh
+++ testapi_supabase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 tar -jxvf ./supabase/volumes/db/data-test.tar.bz2 -C ./supabase/volumes/db/
-docker compose -f docker-compose-test-supabase.yml up --build -d
-docker compose -f docker-compose-test-supabase.yml exec testapi pytest -vv app/tests/ # --cov --cov-branch --cov-report=term-missing --cov-config=app/tests/.coveragerc
-docker compose -f docker-compose-test-supabase.yml down -v
-sudo rm -r ./supabase/volumes/db/data-test
+docker compose -f docker-compose-supabase-test.yml up --build -d
+docker compose -f docker-compose-supabase-test.yml exec testapi pytest -vv app/tests/ # --cov --cov-branch --cov-report=term-missing --cov-config=app/tests/.coveragerc
+# docker compose -f docker-compose-test-supabase.yml down -v
+# sudo rm -r ./supabase/volumes/db/data-test
