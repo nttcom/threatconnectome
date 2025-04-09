fc0b8858a901865e4e0ae2c660e42362f3c49f7b
shihakata <tomokatsu.takahashi@ntt.com>
2025-04-09 10:18:45 +0900

    add check request format and relationship

--- api/app/routers/vulns.py
+++ api/app/routers/vulns.py
@@ -29,6 +29,13 @@ def update_vuln(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="Cannot create default vuln"
             )
 
+        # check request format
+        if (request.title is None) or (request.detail is None):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Both 'title' and 'detail' are required when creating a vuln.",
+            )
+
         # check packages
         requested_packages = {}
         for vulneraable_package in request.vulnerable_packages:
