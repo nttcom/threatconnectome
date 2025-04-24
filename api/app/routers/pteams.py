25d9f48c8283288bc19296263b324c4dc6c05224
TsurutaYoshiki <142189177+TsurutaYoshiki@users.noreply.github.com>
2025-04-24 10:25:45 +0900

    bugfix/fix conflict (#691)

--- api/app/routers/pteams.py
+++ api/app/routers/pteams.py
@@ -1077,7 +1077,7 @@ def upload_pteam_tags_file(
         db.flush()
 
     try:
-        apply_service_tags(db, service_model, json_lines, auto_create_tags=force_mode)
+        apply_service_packages(db, service_model, json_lines)
     except ValueError as err:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
 
