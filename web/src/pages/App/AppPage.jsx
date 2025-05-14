c1433a4f6019762120b0b431eb340b51dcb85f9d
Shikou <shiko.dejima.uw@hitachi-solutions.com>
2025-05-14 14:57:06 +0900

    add resetKeys in ErrorBoundary

--- web/src/pages/App/AppPage.jsx
+++ web/src/pages/App/AppPage.jsx
@@ -49,7 +49,7 @@ export function App() {
       <Main open={system.drawerOpen}>
         <Box display="flex" flexDirection="row" flexGrow={1} justifyContent="center" m={1}>
           <Box display="flex" flexDirection="column" flexGrow={1} maxWidth={mainMaxWidth}>
-            <ErrorBoundary FallbackComponent={AppFallback}>
+            <ErrorBoundary FallbackComponent={AppFallback} resetKeys={[location.pathname]}>
               <OutletWithCheckedParams />
             </ErrorBoundary>
           </Box>
