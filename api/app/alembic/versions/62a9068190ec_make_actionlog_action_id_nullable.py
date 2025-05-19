1a4ee449fa85986d210a6b76d3635fcda1af1577
chisaka12 <t.chisaka@ntt.com>
2025-05-19 16:47:43 +0900

    add migration script to make action_id nullable

---
+++ api/app/alembic/versions/62a9068190ec_make_actionlog_action_id_nullable.py
@@ -0,0 +1,25 @@
+"""make_actionlog.action_id__nullable
+
+Revision ID: 62a9068190ec
+Revises: 68d85e0499fe
+Create Date: 2025-05-19 16:22:31.615601
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "62a9068190ec"
+down_revision = "b842fabcecf2"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.alter_column("actionlog", "action_id", existing_type=sa.Text(), nullable=True)
+
+
+def downgrade() -> None:
+    op.alter_column("actionlog", "action_id", existing_type=sa.Text(), nullable=False)
