ce9b9692e74960775f8bf258009ce46710efd0c6
chisaka12 <t.chisaka@ntt.com>
2025-05-26 15:59:48 +0900

    delete pickAffectedVersions

--- web/src/utils/topicUtils.js
+++ web/src/utils/topicUtils.js
@@ -1,24 +1,6 @@
 import { pickParentTagName } from "./func";
 import { parseVulnerableVersions, versionMatch } from "./versions";
 
-export function pickAffectedVersions(vulnActions, packageName) {
-  const versions = pickAffectedVersionsInner(vulnActions, packageName);
-
-  const parentTagName = pickParentTagName(packageName);
-  if (parentTagName == null) {
-    if (versions.length == 0) {
-      return ["?"];
-    }
-    return [...new Set(versions)].sort();
-  }
-
-  const paretntVersions = pickAffectedVersionsInner(vulnActions, parentTagName);
-  if (versions.length == 0 && paretntVersions.length == 0) {
-    return ["?"];
-  }
-  return [...new Set(versions.concat(paretntVersions))].sort();
-}
-
 function pickAffectedVersionsInner(vulnActions, packageName) {
   return vulnActions.reduce(
     (ret, action) => [...ret, ...(action.ext?.vulnerable_versions?.[packageName] ?? [])],
