a55a2638ed32523ee0f5cceaddcb4d1b3c172c02
chisaka12 <t.chisaka@ntt.com>
2025-06-24 09:27:24 +0900

    delete package

--- api/app/routers/vulns.py
+++ api/app/routers/vulns.py
@@ -376,7 +376,7 @@ def get_vulns(
                 affected_versions=affect.affected_versions,
                 fixed_versions=affect.fixed_versions,
             )
-            for affect, package in affects
+            for affect in affects
         ]
         response_vulns.append(
             schemas.VulnResponse(
