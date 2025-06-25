092dcfd56dcf5ac1f92a1dbac40efe65546a9111
yuki-murasawa <yuki.murasawa@ntt.com>
2025-06-25 09:29:19 +0900

    feat(Status): Make service details responsive Replace `PTeamServiceDetails` with `PTeamServiceDetailsResponsive`

---
+++ web/src/pages/Status/PTeamServiceDetailsMobile.jsx
@@ -0,0 +1,12 @@
+import { Box, Card, CardContent, CardMedia } from "@mui/material";
+
+export function PTeamServiceDetailsMobile() {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
+      <Card sx={{ width: 400 }}>
+        <CardMedia component="img" image="/images/720x480.png" sx={{ aspectRatio: "3 / 2" }} />
+        <CardContent>PTeamServiceDetailsMobile</CardContent>
+      </Card>
+    </Box>
+  );
+}
---
+++ web/src/pages/Status/PTeamServiceDetailsResponsive.jsx
@@ -0,0 +1,11 @@
+import { useTheme, useMediaQuery } from "@mui/material";
+
+import { PTeamServiceDetails } from "./PTeamServiceDetails";
+import { PTeamServiceDetailsMobile } from "./PTeamServiceDetailsMobile";
+
+export function PTeamServiceDetailsResponsive(props) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  return isMobile ? <PTeamServiceDetailsMobile {...props} /> : <PTeamServiceDetails {...props} />;
+}
