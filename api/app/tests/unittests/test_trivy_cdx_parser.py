45cc380b784817dffa5cb9c54425799d5a2ece6a
chisaka12 <t.chisaka@ntt.com>
2025-07-07 17:51:30 +0900

    add test_it_should_unescape_purl_and_extract_correct_package_name_and_ecosystem

---
+++ api/app/tests/unittests/test_trivy_cdx_parser.py
@@ -0,0 +1,57 @@
+from app.sbom.parser.sbom_info import SBOMInfo
+from app.sbom.parser.trivy_cdx_parser import TrivyCDXParser
+
+
+class TestTrivyCDXParser:
+    def test_it_should_unescape_purl_and_extract_correct_package_name_and_ecosystem(self):
+        sbom = {
+            "metadata": {
+                "component": {
+                    "bom-ref": "root-app",
+                    "type": "application",
+                    "name": "sample target1",
+                }
+            },
+            "components": [
+                {
+                    "bom-ref": "root-app",
+                    "type": "application",
+                    "name": "sample target1",
+                    "properties": [
+                        {"name": "aquasecurity:trivy:Type", "value": "npm"},
+                        {"name": "aquasecurity:trivy:Class", "value": "lang-pkgs"},
+                    ],
+                },
+                {
+                    "bom-ref": "pkg:npm/%40babel/code-frame@7.0.0",
+                    "type": "library",
+                    "name": "@babel/code-frame",
+                    "version": "7.0.0",
+                    "purl": "pkg:npm/%40babel/code-frame@7.0.0",
+                    "group": "",
+                    "properties": [
+                        {"name": "aquasecurity:trivy:PkgID", "value": "@babel/code-frame@7.0.0"},
+                        {"name": "aquasecurity:trivy:Type", "value": "npm"},
+                    ],
+                },
+            ],
+            "dependencies": [
+                {
+                    "ref": "root-app",
+                    "dependsOn": ["pkg:npm/%40babel/code-frame@7.0.0"],
+                }
+            ],
+        }
+        sbom_info = SBOMInfo(
+            spec_name="CycloneDX",
+            spec_version="1.5",
+            tool_name="trivy",
+            tool_version="0.52.0",
+        )
+        artifacts = TrivyCDXParser.parse_sbom(sbom, sbom_info)
+        assert len(artifacts) == 1
+        artifact = artifacts[0]
+        assert artifact.package_name == "@babel/code-frame"
+        assert artifact.ecosystem == "npm"
+        assert artifact.package_manager == "npm"
+        assert "7.0.0" in artifact.versions
