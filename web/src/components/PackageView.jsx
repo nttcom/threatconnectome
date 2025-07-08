7a26a884af615a6cd178b7e0e871eee45373c4de
Yuki Murasawa <148930319+yuki-murasawa@users.noreply.github.com>
2025-07-09 01:41:45 +0900

    fix(VulnerabilityDrawerView): Adjust Box width for better responsiveness

--- web/src/components/PackageView.jsx
+++ web/src/components/PackageView.jsx
@@ -1,5 +1,5 @@
 import { Recommend as RecommendIcon, Warning as WarningIcon } from "@mui/icons-material";
-import { Box, Card, Typography, Tooltip } from "@mui/material";
+import { Box, Card, Typography, Tooltip, Grid } from "@mui/material";
 import { green, yellow } from "@mui/material/colors";
 import PropTypes from "prop-types";
 
@@ -13,62 +13,50 @@ export function PackageView(props) {
     <Card
       key={vulnPackage.affected_name + vulnPackage.ecosystem}
       variant="outlined"
-      display="flex"
       sx={{ m: 1, p: 2 }}
     >
       {/* Title -- package name */}
       <Typography variant="h5">{nameWithEcosystem}</Typography>
-      <Box display="flex" flexDirection="row" justifyContent="center">
-        {/* left half -- affected versions */}
-        <Box
-          alignItems="flexStart"
-          display="flex"
-          flexDirection="column"
-          sx={{ width: "50%", minWidth: "50%" }}
-        >
+      <Grid container>
+        {/* affected versions */}
+        <Grid item xs={12} md={6} sx={{ display: "flex", alignItems: "center" }}>
           {affectedVersions.length > 0 ? (
             affectedVersions.map((affectedVersion) => (
               <Box
                 key={affectedVersion}
-                alignItems="center"
-                display="flex"
-                flexDirection="row"
-                sx={{ ml: 2 }}
+                sx={{ display: "flex", alignItems: "center", minWidth: 0 }}
               >
                 <WarningIcon sx={{ fontSize: 32, color: yellow[900] }} />
                 <Tooltip title={affectedVersion} placement="right">
-                  <Typography noWrap sx={{ fontSize: 32, mx: 2 }}>
+                  <Typography
+                    noWrap
+                    sx={{
+                      fontSize: 32,
+                      mx: 2,
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      display: "block",
+                    }}
+                  >
                     {affectedVersion}
                   </Typography>
                 </Tooltip>
               </Box>
             ))
           ) : (
-            <Box alignItems="center" display="flex" flexDirection="row" sx={{ ml: 2 }}>
+            <Box sx={{ display: "flex", alignItems: "center", ml: 2 }}>
               <WarningIcon sx={{ fontSize: 32, color: yellow[900] }} />
               <Typography noWrap sx={{ fontSize: 32, mx: 2 }}>
                 -
               </Typography>
             </Box>
           )}
-        </Box>
-
-        {/* right half -- patched versions */}
-        <Box
-          alignItems="flexStart"
-          display="flex"
-          flexDirection="column"
-          sx={{ width: "50%", minWidth: "50%" }}
-        >
+        </Grid>
+        {/* patched versions */}
+        <Grid item xs={12} md={6} sx={{ display: "flex", alignItems: "center" }}>
           {fixedVersions.length > 0 ? (
             fixedVersions.map((fixedVersion) => (
-              <Box
-                key={fixedVersion}
-                alignItems="center"
-                display="flex"
-                flexDirection="row"
-                sx={{ ml: 2 }}
-              >
+              <Box key={fixedVersion} sx={{ display: "flex", alignItems: "center" }}>
                 <RecommendIcon sx={{ fontSize: 32, color: green[500] }} />
                 <Tooltip title={fixedVersion} placement="right">
                   <Typography noWrap sx={{ fontSize: 32, mx: 2 }}>
@@ -78,15 +66,15 @@ export function PackageView(props) {
               </Box>
             ))
           ) : (
-            <Box alignItems="center" display="flex" flexDirection="row" sx={{ ml: 2 }}>
+            <Box alignItems="center" display="flex" flexDirection="row">
               <RecommendIcon sx={{ fontSize: 32, color: green[500] }} />
               <Typography noWrap sx={{ fontSize: 32, mx: 2 }}>
                 -
               </Typography>
             </Box>
           )}
-        </Box>
-      </Box>
+        </Grid>
+      </Grid>
     </Card>
   );
 }
