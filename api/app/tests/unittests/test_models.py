67f15b985730cf6858a87cd377682969b1c644f4
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2025-07-15 00:16:05 +0000

    delete package_type

--- api/app/tests/unittests/test_models.py
+++ api/app/tests/unittests/test_models.py
@@ -5,18 +5,16 @@ from app import models
 
 class TestVulnMatchingEcosystem:
     @pytest.mark.parametrize(
-        "ecosystem, package_type, expected",
+        "ecosystem, expected",
         [
-            ("alpine-3.22.0", "OS", "alpine-3.22"),
-            ("alpine-3", "OS", "alpine-3"),
-            ("alpine-test-3.22.0", "OS", "alpine-test-3.22.0"),
-            ("alpine", "OS", "alpine"),
-            ("ubuntu-20.04", "OS", "ubuntu-20.04"),
+            ("alpine-3.22.0", "alpine-3.22"),
+            ("alpine-3", "alpine-3"),
+            ("alpine-test-3.22.0", "alpine-test-3.22.0"),
+            ("alpine", "alpine"),
+            ("ubuntu-20.04", "ubuntu-20.04"),
         ],
     )
-    def test_it_should_return_expected_value_for_os_package(
-        self, ecosystem: str, package_type: str, expected: str
-    ):
+    def test_it_should_return_expected_value_for_os_package(self, ecosystem: str, expected: str):
         # Given
         os_package = models.OSPackage(
             name="test_package", ecosystem=ecosystem, source_name="test_source_name_package"
@@ -26,15 +24,13 @@ class TestVulnMatchingEcosystem:
         assert os_package.vuln_matching_ecosystem == expected
 
     @pytest.mark.parametrize(
-        "ecosystem, package_type, expected",
+        "ecosystem, expected",
         [
-            ("npm", "LANG", "npm"),
-            ("pypi", "LANG", "pypi"),
+            ("npm", "npm"),
+            ("pypi", "pypi"),
         ],
     )
-    def test_it_should_return_expected_value_for_lang_package(
-        self, ecosystem: str, package_type: str, expected: str
-    ):
+    def test_it_should_return_expected_value_for_lang_package(self, ecosystem: str, expected: str):
         # Given
         lang_package = models.LangPackage(name="test_package", ecosystem=ecosystem)
 
