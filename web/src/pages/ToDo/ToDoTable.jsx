379cb87a30b578e5bacd72405c672a8db2052c00
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2025-07-31 04:37:24 +0000

    fix ui

--- web/src/pages/ToDo/ToDoTable.jsx
+++ web/src/pages/ToDo/ToDoTable.jsx
@@ -29,7 +29,7 @@ export function ToDoTable({ myTasks, pteamIds, cveIds, page, setPage }) {
     pteamIds,
     offset: page * rowsPerPage,
     limit: rowsPerPage,
-    sortKey: "ssvc_deployer_priority_desc",
+    sortKey: "-ssvc_deployer_priority",
     assignedToMe: myTasks,
     excludeStatuses: ["completed"],
     cveIds: cveIds,
