8a8f43e8c4aa5ea4100e9acbc2799026856ae2ba
Yuki Murasawa <148930319+yuki-murasawa@users.noreply.github.com>
2025-11-04 09:40:15 +0900

    feat: Storybookのセットアップ

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,11 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+
+const dirname =
+  typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   build: {
