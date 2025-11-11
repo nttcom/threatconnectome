6cb5e292e892d50a8413542ffb38c8f8b2330220
Yuki Murasawa <148930319+yuki-murasawa@users.noreply.github.com>
2025-11-12 08:06:56 +0900

    feat: PackagePageのユニットテストを強化し、API呼び出しのモックを追加

--- web/src/utils/__tests__/test-utils.jsx
+++ web/src/utils/__tests__/test-utils.jsx
@@ -1,22 +1,44 @@
+import { configureStore } from "@reduxjs/toolkit";
 import { render as rtlRender } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 import { AuthProvider } from "../../providers/auth/AuthContext";
-import store from "../../store";
+import { tcApi } from "../../services/tcApi";
+import { sliceReducers } from "../../slices";
 
-const AllTheProviders = ({ children }) => {
-  return (
-    <Provider store={store}>
-      <MemoryRouter>
-        <AuthProvider>{children}</AuthProvider>
-      </MemoryRouter>
-    </Provider>
-  );
-};
+const render = (
+  ui,
+  {
+    preloadedState = { auth: { authUserIsReady: true } },
+    store = configureStore({
+      reducer: {
+        ...sliceReducers,
+        [tcApi.reducerPath]: tcApi.reducer,
+      },
+      middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tcApi.middleware),
+      // preloadedState,
+    }),
 
-const render = (ui, options) => {
-  rtlRender(ui, { wrapper: AllTheProviders, ...options });
+    route = "/",
+    path = "/",
+    ...options
+  } = {},
+) => {
+  const AllTheProviders = ({ children }) => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[route]}>
+          <AuthProvider>
+            <Routes>
+              <Route path={path} element={children} />
+            </Routes>
+          </AuthProvider>
+        </MemoryRouter>
+      </Provider>
+    );
+  };
+  return rtlRender(ui, { wrapper: AllTheProviders, ...options });
 };
 
 export * from "@testing-library/react";
