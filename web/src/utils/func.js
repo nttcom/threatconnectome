2e39312ca30c08e2ba74bf7b5609968dc5e35546
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2025-12-12 07:57:03 +0000

    add maskPhoneNumber

--- web/src/utils/func.js
+++ web/src/utils/func.js
@@ -1,3 +1,4 @@
+import parsePhoneNumber from "libphonenumber-js";
 import { cvssRatings } from "./const";
 
 export const a11yProps = (index) => ({
@@ -149,4 +150,17 @@ export const countFullWidthAndHalfWidthCharacters = (string) => {
   }
 
   return count;
+};
+
+export const maskPhoneNumber = (inputPhoneNumber) => {
+  const phoneNumber = parsePhoneNumber(inputPhoneNumber);
+
+  if (phoneNumber && phoneNumber.isValid()) {
+    const countryCode = `+${phoneNumber.countryCallingCode}`;
+    const nationalNumber = phoneNumber.nationalNumber;
+    const lastFourDigits = nationalNumber.slice(-4);
+    return `${countryCode} ${"*".repeat(nationalNumber.length - 4)}${lastFourDigits}`;
+  } else {
+    return "N/A";
+  }
 };
