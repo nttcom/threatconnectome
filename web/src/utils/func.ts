44dcad4e41831af3a61bcf84469a1301d769768a
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2025-12-18 02:20:34 +0000

    topic/func-TypeScript-migration

--- web/src/utils/func.ts
+++ web/src/utils/func.ts
@@ -1,14 +1,31 @@
+// @ts-expect-error TS7016
 import { cvssRatings } from "./const";
 
-export const a11yProps = (index) => ({
+import { UserResponse, TicketResponse } from "../../types/types.gen.ts";
+
+type SSVCPriority =
+  | "immediate"
+  | "Immediate"
+  | "out_of_cycle"
+  | "Out-of-cycle"
+  | "scheduled"
+  | "Scheduled"
+  | "defer"
+  | "Defer"
+  | "safe"
+  | "Safe"
+  | "empty"
+  | "Empty";
+
+export const a11yProps = (index: number) => ({
   id: `tab-${index}`,
   "aria-controls": `tabpanel-${index}`,
 });
 
-export const calcTimestampDiff = (timestamp) => {
+export const calcTimestampDiff = (timestamp: string) => {
   if (!timestamp) return "-";
   const targetDate = new Date(timestamp);
-  const daysAgo = Math.floor((new Date() - targetDate) / (1000 * 60 * 60 * 24));
+  const daysAgo = Math.floor((new Date().getTime() - targetDate.getTime()) / (1000 * 60 * 60 * 24));
   switch (daysAgo) {
     case 0:
       return "today";
@@ -19,7 +36,10 @@ export const calcTimestampDiff = (timestamp) => {
   }
 };
 
-export const utcStringToLocalDate = (utcString, includeTimezone) => {
+export const utcStringToLocalDate = (
+  utcString: string | null | undefined,
+  includeTimezone: boolean,
+) => {
   if (!utcString) return null;
   const date = new Date(utcString);
   const year = date.getFullYear();
@@ -42,7 +62,7 @@ export const utcStringToLocalDate = (utcString, includeTimezone) => {
   return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}${offsetStr}`;
 };
 
-export const errorToString = (error) => {
+export const errorToString = (error: string | Record<string, any>) => {
   if (typeof error === "string") return error;
   if (error.status && error.data?.detail) {
     // RTKQ
@@ -58,18 +78,18 @@ export const errorToString = (error) => {
   return JSON.stringify(error); // not expected case
 };
 
-export const setEquals = (set1, set2) =>
+export const setEquals = (set1: Set<string>, set2: Set<string>) =>
   set1.size === set2.size && Array.from(set1).every((val) => set2.has(val));
 
-export const blobToDataURL = async (blob) =>
+export const blobToDataURL = async (blob: Blob) =>
   new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onload = (event) => resolve(event.target.result);
+    reader.onload = (event) => resolve(event.target?.result);
     reader.onerror = (error) => reject(error);
     reader.readAsDataURL(blob);
   });
 
-export const compareSSVCPriority = (prio1, prio2) => {
+export const compareSSVCPriority = (prio1: SSVCPriority, prio2: SSVCPriority) => {
   const toIntDict = {
     immediate: 1,
     Immediate: 1,
@@ -90,13 +110,15 @@ export const compareSSVCPriority = (prio1, prio2) => {
   else return 1;
 };
 
-export const searchWorstSSVC = (tickets) => {
+export const searchWorstSSVC = (tickets: Array<TicketResponse>) => {
   if (!tickets || tickets.length === 0) {
     return null;
   }
 
   const result = tickets.reduce((worstSSVC, ticket) => {
-    if (compareSSVCPriority(worstSSVC, ticket.ssvc_deployer_priority) === 1) {
+    const currentPrio = ticket.ssvc_deployer_priority ?? "empty";
+    const worstPrio = worstSSVC ?? "empty";
+    if (compareSSVCPriority(worstPrio, currentPrio) === 1) {
       return ticket.ssvc_deployer_priority;
     }
     return worstSSVC;
@@ -105,8 +127,13 @@ export const searchWorstSSVC = (tickets) => {
   return result;
 };
 
-export const cvssConvertToName = (cvssScore) => {
+export const cvssConvertToName = (cvssScore: number | string) => {
   let rating;
+
+  if (typeof cvssScore === "string") {
+    return "None";
+  }
+
   if (0 < cvssScore && cvssScore < 4.0) {
     rating = "Low";
   } else if (4.0 <= cvssScore && cvssScore < 7.0) {
@@ -121,7 +148,7 @@ export const cvssConvertToName = (cvssScore) => {
   return rating;
 };
 
-export const cvssConvertToScore = (cvssName) => {
+export const cvssConvertToScore = (cvssName: string) => {
   const rating = cvssRatings[cvssName];
   if (rating) {
     return [rating.min, rating.max];
@@ -129,16 +156,15 @@ export const cvssConvertToScore = (cvssName) => {
   return [undefined, undefined];
 };
 
-export const checkAdmin = (member, pteamId) => {
+export const checkAdmin = (member: UserResponse, pteamId: string) => {
   return member.pteam_roles.some(
     (pteam_role) => pteam_role.pteam.pteam_id === pteamId && pteam_role.is_admin,
   );
 };
 
-export const countFullWidthAndHalfWidthCharacters = (string) => {
+export const countFullWidthAndHalfWidthCharacters = (string: string) => {
   let count = 0;
   for (let i = 0; i < string.length; i++) {
-    const char = string[i];
     if (string[i].match(/[ -~｡-ﾟ]/)) {
       // half-width characters
       count += 1;
