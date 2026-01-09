b43b14ff7dd491096ff4d2d3775d0387bfa411e7
Shikou <shiko.dejima.uw@hitachi-solutions.com>
2026-01-09 13:28:59 +0900

    fix create ecosystem_eol_dependency by service

--- api/app/business/eol/eol_business.py
+++ api/app/business/eol/eol_business.py
@@ -14,21 +14,24 @@ def fix_eol_dependency_by_eol_product(db: Session, eol_product: models.EoLProduc
 def fix_eol_dependency_by_service(db: Session, service: models.Service) -> None:
     _delete_eol_dependency_by_service(db, service)
 
-    related_services = set()
+    related_service_pairs = set()
+
     for dependency in service.dependencies:
         package_version = dependency.package_version
         eol_versions = command.get_related_eol_versions_by_package_version(db, package_version)
         for eol_version in eol_versions:
             if eol_version.eol_product.is_ecosystem:
-                related_services.add(dependency.service)
+                related_service_pairs.add(
+                    (eol_version.eol_version_id, dependency.service.service_id)
+                )
             else:
                 package_eol_business.create_package_eol_dependency_if_not_exists(
                     db, eol_version.eol_version_id, dependency.dependency_id
                 )
 
-    for service in related_services:
+    for eol_version_id, service_id in related_service_pairs:
         ecosystem_eol_business.create_ecosystem_eol_dependency_if_not_exists(
-            db, eol_version.eol_version_id, service.service_id
+            db, eol_version_id, service_id
         )
 
 
