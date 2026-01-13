13c4b3a73f5333d01150f369b473b05792bb2330
TsurutaYoshiki <yoshiki.tsrututa.ak@hitachi-solutions.com>
2026-01-13 06:31:59 +0000

    topic/fix test_eols

--- api/app/tests/requests/test_eols.py
+++ api/app/tests/requests/test_eols.py
@@ -1,14 +1,15 @@
-from datetime import date, datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
 from typing import Any
 from uuid import uuid4
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from app import models, persistence
+from app import models
 from app.main import app
+from app.routers.pteams import bg_create_tags_from_sbom_json
 from app.tests.medium.constants import PTEAM1, USER1
 from app.tests.medium.utils import (
     create_pteam,
@@ -321,10 +322,10 @@ class TestGetEolProducts:
             "matching_name": "product_2",
             "eol_versions": [
                 {
-                    "version": "3.0.0",
+                    "version": "20.04",
                     "release_date": "2021-01-01",
                     "eol_from": "2026-01-01",
-                    "matching_version": "3.0.0",
+                    "matching_version": "ubuntu-20.04",
                 }
             ],
         }
@@ -335,150 +336,59 @@ class TestGetEolProducts:
             "product_category": models.ProductCategoryEnum.PACKAGE,
             "description": "product 3 description",
             "is_ecosystem": False,
-            "matching_name": "product_3",
+            "matching_name": "axios",
             "eol_versions": [
                 {
-                    "version": "4.0.0",
+                    "version": "1.6.7",
                     "release_date": "2019-01-01",
                     "eol_from": "2024-01-01",
-                    "matching_version": "4.0.0",
+                    "matching_version": "1.6.7",
                 }
             ],
         }
 
-        # Todo: After creating the PUT API, modify the API.
         self.current_time = datetime.now(timezone.utc)
-        eol_product_1 = models.EoLProduct(
-            eol_product_id=str(self.eol_product_id_1),
-            name=self.eol_product_1_request["name"],
-            product_category=self.eol_product_1_request["product_category"],
-            description=self.eol_product_1_request["description"],
-            is_ecosystem=self.eol_product_1_request["is_ecosystem"],
-            matching_name=self.eol_product_1_request["matching_name"],
+        client.put(
+            f"/eols/{self.eol_product_id_1}",
+            headers=headers_with_api_key(USER1),
+            json=self.eol_product_1_request,
         )
-        persistence.create_eol_product(testdb, eol_product_1)
-
-        eol_product_2 = models.EoLProduct(
-            eol_product_id=str(self.eol_product_id_2),
-            name=self.eol_product_2_request["name"],
-            product_category=self.eol_product_2_request["product_category"],
-            description=self.eol_product_2_request["description"],
-            is_ecosystem=self.eol_product_2_request["is_ecosystem"],
-            matching_name=self.eol_product_2_request["matching_name"],
+        client.put(
+            f"/eols/{self.eol_product_id_2}",
+            headers=headers_with_api_key(USER1),
+            json=self.eol_product_2_request,
         )
-        persistence.create_eol_product(testdb, eol_product_2)
-
-        eol_product_3 = models.EoLProduct(
-            eol_product_id=str(self.eol_product_id_3),
-            name=self.eol_product_3_request["name"],
-            product_category=self.eol_product_3_request["product_category"],
-            description=self.eol_product_3_request["description"],
-            is_ecosystem=self.eol_product_3_request["is_ecosystem"],
-            matching_name=self.eol_product_3_request["matching_name"],
+        client.put(
+            f"/eols/{self.eol_product_id_3}",
+            headers=headers_with_api_key(USER1),
+            json=self.eol_product_3_request,
         )
-        persistence.create_eol_product(testdb, eol_product_3)
 
-        # Create EoL versions for product 1
-        now = datetime.now(timezone.utc)
-        self.eol_version_1_1 = models.EoLVersion(
-            eol_product_id=str(self.eol_product_id_1),
-            version=self.eol_product_1_request["eol_versions"][0]["version"],
-            release_date=date.fromisoformat(
-                self.eol_product_1_request["eol_versions"][0]["release_date"]
-            ),
-            eol_from=date.fromisoformat(self.eol_product_1_request["eol_versions"][0]["eol_from"]),
-            matching_version=self.eol_product_1_request["eol_versions"][0]["matching_version"],
-            created_at=now,
-            updated_at=now,
+        # Add package data to match with self.eol_product_2_request
+        service_name1 = "test_service1"
+        upload_file_name1 = "trivy-ubuntu2004.cdx.json"
+        sbom_file1 = (
+            Path(__file__).resolve().parent.parent / "common" / "upload_test" / upload_file_name1
         )
-        persistence.create_eol_version(testdb, self.eol_version_1_1)
+        with open(sbom_file1, "r") as sbom:
+            sbom_json1 = sbom.read()
 
-        self.eol_version_1_2 = models.EoLVersion(
-            eol_product_id=str(self.eol_product_id_1),
-            version=self.eol_product_1_request["eol_versions"][1]["version"],
-            release_date=date.fromisoformat(
-                self.eol_product_1_request["eol_versions"][1]["release_date"]
-            ),
-            eol_from=date.fromisoformat(self.eol_product_1_request["eol_versions"][1]["eol_from"]),
-            matching_version=self.eol_product_1_request["eol_versions"][1]["matching_version"],
-            created_at=now,
-            updated_at=now,
+        bg_create_tags_from_sbom_json(
+            sbom_json1, self.pteam1.pteam_id, service_name1, upload_file_name1
         )
-        persistence.create_eol_version(testdb, self.eol_version_1_2)
 
-        # Create EoL versions for product 2
-        self.eol_version_2_1 = models.EoLVersion(
-            eol_product_id=str(self.eol_product_id_2),
-            version=self.eol_product_2_request["eol_versions"][0]["version"],
-            release_date=date.fromisoformat(
-                self.eol_product_2_request["eol_versions"][0]["release_date"]
-            ),
-            eol_from=date.fromisoformat(self.eol_product_2_request["eol_versions"][0]["eol_from"]),
-            matching_version=self.eol_product_2_request["eol_versions"][0]["matching_version"],
-            created_at=now,
-            updated_at=now,
+        # Add package data to match with self.eol_product_3_request
+        service_name2 = "test_service2"
+        upload_file_name2 = "test_trivy_cyclonedx_axios.json"
+        sbom_file2 = (
+            Path(__file__).resolve().parent.parent / "common" / "upload_test" / upload_file_name2
         )
-        persistence.create_eol_version(testdb, self.eol_version_2_1)
+        with open(sbom_file2, "r") as sbom:
+            sbom_json2 = sbom.read()
 
-        # Create EoL versions for product 3
-        self.eol_version_3_1 = models.EoLVersion(
-            eol_product_id=str(self.eol_product_id_3),
-            version=self.eol_product_3_request["eol_versions"][0]["version"],
-            release_date=date.fromisoformat(
-                self.eol_product_3_request["eol_versions"][0]["release_date"]
-            ),
-            eol_from=date.fromisoformat(self.eol_product_3_request["eol_versions"][0]["eol_from"]),
-            matching_version=self.eol_product_3_request["eol_versions"][0]["matching_version"],
-            created_at=now,
-            updated_at=now,
+        bg_create_tags_from_sbom_json(
+            sbom_json2, self.pteam1.pteam_id, service_name2, upload_file_name2
         )
-        persistence.create_eol_version(testdb, self.eol_version_3_1)
-
-        # Create Service for pteam1
-        self.service1 = models.Service(
-            pteam_id=str(self.pteam1.pteam_id),
-            service_name="test_service",
-        )
-        testdb.add(self.service1)
-        testdb.flush()
-
-        # Create EcosystemEoLDependency to link service to eol_version (product 2)
-        ecosystem_dependency = models.EcosystemEoLDependency(
-            service_id=self.service1.service_id,
-            eol_version_id=str(self.eol_version_2_1.eol_version_id),
-        )
-        testdb.add(ecosystem_dependency)
-
-        # Create Package, PackageVersion, Dependency and PackageEoLDependency to link to product 3
-        self.package1 = models.Package(
-            name="test_package",
-            ecosystem="npm",
-        )
-        persistence.create_package(testdb, self.package1)
-
-        self.package_version1 = models.PackageVersion(
-            package_id=self.package1.package_id,
-            version="1.0.0",
-        )
-        persistence.create_package_version(testdb, self.package_version1)
-
-        self.dependency1 = models.Dependency(
-            target="test_target",
-            package_manager="npm",
-            package_version_id=self.package_version1.package_version_id,
-            service=self.service1,
-        )
-        testdb.add(self.dependency1)
-        testdb.flush()
-
-        # Create PackageEoLDependency to link dependency to eol_version (product 3)
-        package_eol_dependency = models.PackageEoLDependency(
-            dependency_id=self.dependency1.dependency_id,
-            eol_version_id=str(self.eol_version_3_1.eol_version_id),
-        )
-        testdb.add(package_eol_dependency)
-
-        testdb.commit()
 
     def test_get_all_eol_products(self):
         # When: Get all products without filters
@@ -693,55 +603,31 @@ class TestGetEolProducts:
 
 class TestDeleteEol:
     @pytest.fixture(scope="function", autouse=True)
-    def common_setup(self, testdb: Session):
+    def common_setup(self):
         self.user1 = create_user(USER1)
-        # Todo: After creating the PUT API, modify the API.
         self.new_eol_product_id = uuid4()
-        eol_product = models.EoLProduct(
-            eol_product_id=str(self.new_eol_product_id),
-            name="test_eol_product",
-            product_category=models.ProductCategoryEnum.PACKAGE,
-            description="test_description",
-            is_ecosystem=False,
-            matching_name="test_matching_name",
+        self.request1 = {
+            "name": "test_eol_product",
+            "product_category": models.ProductCategoryEnum.PACKAGE,
+            "description": "test_description",
+            "is_ecosystem": False,
+            "matching_name": "test_matching_name",
+            "eol_versions": [
+                {
+                    "version": "2.0.0",
+                    "release_date": "2020-01-02",
+                    "eol_from": "2030-01-02",
+                    "matching_version": "2.0.0",
+                }
+            ],
+        }
+
+        # Create a eol to delete
+        client.put(
+            f"/eols/{self.new_eol_product_id}",
+            headers=headers_with_api_key(USER1),
+            json=self.request1,
         )
-
-        persistence.create_eol_product(testdb, eol_product)
-
-        now = datetime.now(timezone.utc)
-        eol_version_model = models.EoLVersion(
-            eol_product_id=str(self.new_eol_product_id),
-            version="2.0.0",
-            release_date=date.fromisoformat("2020-01-02"),
-            eol_from=date.fromisoformat("2030-01-02"),
-            matching_version="2.0.0",
-            created_at=now,
-            updated_at=now,
-        )
-        persistence.create_eol_version(testdb, eol_version_model)
-        testdb.commit()
-        # self.request1 = {
-        #     "name": "test_eol_product",
-        #     "product_category": models.ProductCategoryEnum.PACKAGE,
-        #     "description": "test_description",
-        #     "is_ecosystem": False,
-        #     "matching_name": "test_matching_name",
-        #     "eol_versions": [
-        #         {
-        #             "version": "2.0.0",
-        #             "release_date": "2020-01-02",
-        #             "eol_from": "2030-01-02",
-        #             "matching_version": "2.0.0",
-        #         }
-        #     ],
-        # }
-
-        # # Create a eol to delete
-        # client.put(
-        #     f"/eols/{self.new_eol_product_id}",
-        #     headers=headers_with_api_key(USER1),
-        #     json=self.request1,
-        # )
 
     def test_it_should_delete_eol_when_eol_product_id_exists(self, testdb: Session):
         # When
@@ -751,16 +637,11 @@ class TestDeleteEol:
 
         # Then
         assert response.status_code == 204
-        # Todo: After creating the GET API, modify the API.
-        eol_product = testdb.scalars(
-            select(models.EoLProduct).where(
-                models.EoLProduct.eol_product_id == str(self.new_eol_product_id)
-            )
-        ).one_or_none()
-        assert eol_product is None
-        # get_response = client.get(f"/eols/{self.new_eol_product_id}", headers=headers(USER1))
-        # assert get_response.status_code == 404  # Not Found
-        # assert get_response.json()["detail"] == "No such eol"
+        get_response = client.get(
+            f"/eols?eol_product_id={self.new_eol_product_id}", headers=headers(USER1)
+        )
+        assert get_response.status_code == 404
+        assert get_response.json()["detail"] == "No such eol"
 
     def test_it_should_return_404_when_eol_product_id_does_not_exist(self):
         # Given
