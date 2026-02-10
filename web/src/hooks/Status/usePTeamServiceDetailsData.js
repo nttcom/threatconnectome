5fe2fe5c46731d8b5069b98cb57c084ce99a65db
chisaka12 <t.chisaka@ntt.com>
2026-02-10 10:23:12 +0900

    add fixes to the hooks directory

--- web/src/hooks/Status/usePTeamServiceDetailsData.js
+++ web/src/hooks/Status/usePTeamServiceDetailsData.js
@@ -1,10 +1,12 @@
 import { useGetPTeamServiceThumbnailQuery } from "../../services/tcApi";
 import { systemExposure, missionImpact } from "../../utils/const";
 import { ssvcPriorityProps } from "../../utils/ssvcUtils";
+import { useTranslation } from "react-i18next";
 
 const noImageAvailableUrl = "images/no-image-available-720x480.png";
 
 export function usePTeamServiceDetailsData(pteamId, service, highestSsvcPriority) {
+  const { t } = useTranslation("hooks", { keyPrefix: "PTeamServiceDetails.labels" });
   const {
     data: thumbnail,
     isError: thumbnailIsError,
@@ -18,18 +20,18 @@ export function usePTeamServiceDetailsData(pteamId, service, highestSsvcPriority
 
   const statusItems = [
     {
-      label: "Highest SSVC Priority",
+      label: t("highestSsvcPriority"),
       value: ssvcPriorityProps[highestSsvcPriority]?.displayName || highestSsvcPriority,
     },
     {
-      label: "System Exposure",
+      label: t("systemExposure"),
       value: systemExposure[service.system_exposure] || service.system_exposure,
     },
     {
-      label: "Mission Impact",
+      label: t("missionImpact"),
       value: missionImpact[service.service_mission_impact] || service.service_mission_impact,
     },
-    { label: "Default Safety Impact", value: service.service_safety_impact },
+    { label: t("defaultSafetyImpact"), value: service.service_safety_impact },
   ];
 
   return {
