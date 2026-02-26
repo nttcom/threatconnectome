f33dcee92eb484c63d7050c6c2bcb83f47bc9161
chisaka12 <t.chisaka@ntt.com>
2026-02-26 17:20:23 +0900

    implementation of audit logging (generic API implementation)

--- api/app/utility/api_logging_middleware.py
+++ api/app/utility/api_logging_middleware.py
@@ -5,7 +5,61 @@ from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
 logger = logging.getLogger("api_logger")
-COMMON_API_LIST = ["update_user", "get_dependencies"]
+COMMON_API_LIST = [
+    # users
+    "get_my_user_info",
+    "update_user",
+    "delete_user",
+    # actionlogs
+    "get_logs",
+    "create_log",
+    "get_vuln_logs",
+    # external
+    "check_webhook_url",
+    "check_email",
+    # vulns
+    "update_vuln",
+    "delete_vuln",
+    "get_vuln",
+    "get_vulns",
+    # tickets
+    "get_tickets",
+    "update_insight",
+    "get_insight",
+    "delete_insight",
+    # eols
+    "get_eol_products",
+    "update_eol",
+    "delete_eol",
+    "check_eol_notification",
+    # pteams
+    "apply_invitation",
+    "get_pteam",
+    "force_calculate_ssvc_priority",
+    "get_pteam_services",
+    "update_pteam_service",
+    "get_service_thumbnail",
+    "remove_service_thumbnail",
+    "get_pteam_packages_summary",
+    "get_dependency",
+    "get_vuln_ids_tied_to_service_package",
+    "get_ticket_counts_tied_to_service_package",
+    "get_tickets_by_service_id_and_package_id_and_vuln_id",
+    "get_ticket",
+    "update_ticket",
+    "create_pteam",
+    "remove_service",
+    "update_pteam",
+    "get_pteam_members",
+    "update_pteam_member",
+    "delete_member",
+    "create_invitation",
+    "list_invitations",
+    "delete_invitation",
+    "delete_pteam",
+    "get_eol_products_with_pteam_id",
+    "get_dependencies",
+]
 UPLOAD_API_LIST = [
     "upload_service_thumbnail",
     "upload_pteam_sbom_file",
@@ -41,7 +95,9 @@ class ApiLoggingMiddleware(BaseHTTPMiddleware):
         elif endpoint_name == CREATE_USER:
             pass
         else:
-            pass
+            user = getattr(request.state, "current_user", None)
+            if user:
+                self.create_log_for_common_api(request, response, body_bytes)
 
         return response
 
