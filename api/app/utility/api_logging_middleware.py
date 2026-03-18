f924344cc9b8d7aaefd0b75b46d04c41dd2d6ab8
TsurutaYoshiki <142189177+TsurutaYoshiki@users.noreply.github.com>
2026-03-18 11:45:17 +0900

    topic/add audit log (#1197)

--- api/app/utility/api_logging_middleware.py
+++ api/app/utility/api_logging_middleware.py
@@ -5,62 +5,7 @@ from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
 logger = logging.getLogger("api_logger")
-COMMON_API_LIST = [
-    # users
-    "get_my_user_info",
-    "update_user",
-    "delete_user",
-    # actionlogs
-    "get_logs",
-    "create_log",
-    "get_vuln_logs",
-    # external
-    "check_webhook_url",
-    "check_email",
-    # vulns
-    "update_vuln",
-    "delete_vuln",
-    "get_vuln",
-    "get_vulns",
-    # tickets
-    "get_tickets",
-    "update_insight",
-    "get_insight",
-    "delete_insight",
-    # eols
-    "get_eol_products",
-    "update_eol",
-    "delete_eol",
-    "check_eol_notification",
-    # pteams
-    "apply_invitation",
-    "invited_pteam",
-    "get_pteam",
-    "force_calculate_ssvc_priority",
-    "get_pteam_services",
-    "update_pteam_service",
-    "get_service_thumbnail",
-    "remove_service_thumbnail",
-    "get_pteam_packages_summary",
-    "get_dependencies",
-    "get_dependency",
-    "get_vuln_ids_tied_to_service_package",
-    "get_ticket_counts_tied_to_service_package",
-    "get_tickets_by_service_id_and_package_id_and_vuln_id",
-    "get_ticket",
-    "update_ticket",
-    "create_pteam",
-    "remove_service",
-    "update_pteam",
-    "get_pteam_members",
-    "update_pteam_member",
-    "delete_member",
-    "create_invitation",
-    "list_invitations",
-    "delete_invitation",
-    "delete_pteam",
-    "get_eol_products_with_pteam_id",
-]
+
 UPLOAD_API_LIST = [
     "upload_service_thumbnail",
     "upload_pteam_sbom_file",
@@ -84,16 +29,14 @@ class ApiLoggingMiddleware(BaseHTTPMiddleware):
         route = request.scope.get("route")
         endpoint_name = getattr(route, "name", None) if route else None
 
-        if endpoint_name in COMMON_API_LIST:
-            self.create_log_for_common_api(request, response, body_bytes)
-        elif endpoint_name in UPLOAD_API_LIST:
+        if endpoint_name in UPLOAD_API_LIST:
             self.create_log_for_upload_api(request, response)
         elif endpoint_name in AUTH_API_LIST:
             self.create_log_for_auth_api(request, response)
         elif endpoint_name == CREATE_USER:
             self.create_log_for_create_user(request, response, body_bytes)
         else:
-            pass
+            self.create_log_for_common_api(request, response, body_bytes)
 
         return response
 
