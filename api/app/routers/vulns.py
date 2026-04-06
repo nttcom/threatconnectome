677892480e20775330ebe0d8588dcc69f6e789d2
Shikou <shiko.dejima.uw@hitachi-solutions.com>
2026-04-06 15:37:38 +0900

    fix refresh scope

--- api/app/routers/vulns.py
+++ api/app/routers/vulns.py
@@ -195,7 +195,7 @@ def __handle_update_vuln(
 
     vuln.updated_at = datetime.now(timezone.utc)
     db.flush()
-    db.refresh(vuln)
+    db.refresh(vuln, attribute_names=["affects", "threats"])
 
     new_threats: list[models.Threat] = threat_business.fix_threat_by_vuln(db, vuln)
     for threat in new_threats:
